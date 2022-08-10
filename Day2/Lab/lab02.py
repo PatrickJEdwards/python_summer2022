344dce741ee88c1666e312131c50db9d3216bbea
Patrick Edwards <edwards.p@wustl.edu>
2022-08-10 10:10:32 -0500

    Update lab02.py

--- Day2/Lab/lab02.py
+++ Day2/Lab/lab02.py
@@ -7,14 +7,47 @@ class Clock:
 
     ## Print the time
     def __str__(self):
+        return f"{self.hour:02}" + ":" + f"{self.minutes:02}"
     
     ## Add time
     ## Don't return anything
-    def __add__(self,minutes):
+    def __add__(self, minutes):
+        if (self.minutes + minutes) < 60:
+            print("Add: 1st IF fired")
+            self.minutes += minutes
+        elif (minutes % 60) == 0:
+            print("Add: 2nd IF fired")
+            self.hour += ((self.minutes + minutes) // 60)
+        elif ((self.minutes + minutes) % 60) == 0:
+            print("Add: 3rd IF fired")
+            self.minutes = 0
+        else:
+            print("Add: 4th IF fired")
+            self.hour += ((self.minutes + minutes) // 60)
+            self.minutes += ((self.minutes + minutes) % 60)
+        if self.hour >= 24:
+            print("Add: 24 hour IF fired")
+            self.hour -= 24
     
     ## Subtract time
     ## Don't return anything
-    def __sub__(self,minutes):
+    def __sub__(self, minutes):
+        if (self.minutes - minutes) > 0:
+            print("Sub: 1st IF fired")
+            self.minutes -= minutes
+        elif (minutes % 60) == 0:
+            print("Sub: 2nd IF fired")
+            self.hour -= ((self.minutes - minutes) // 60)
+        elif ((self.minutes - minutes) % 60) == 0:
+            print("Sub: 3rd IF fired")
+            self.minutes = 0
+        else:
+            print("Sub: 4th IF fired")
+            self.hour += ((self.minutes - minutes) // 60)
+            self.minutes += ((self.minutes - minutes) % 60)
+        if self.hour < 0:
+            print("Sub: 24 hour IF fired")
+            self.hour += 24
     
     ## Are two times equal?
     def __eq__(self, other):
