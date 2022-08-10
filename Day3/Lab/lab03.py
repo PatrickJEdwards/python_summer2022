0182a90e29727ea6de6b46c48247110c7003dcbd
Patrick Edwards <edwards.p@wustl.edu>
2022-08-10 13:58:08 -0500

    update

--- Day3/Lab/lab03.py
+++ Day3/Lab/lab03.py
@@ -11,18 +11,73 @@
 
 ## make all characters capitalized
 def shout(txt):
+    if type(txt) in [int, float]:
+        raise TypeError("This is an integer or float. Input should be a single string.")
+    if type(txt) not in [str]:
+        raise TypeError("This is not a string. Input should be a single string.")
+    txt = txt.upper()
+    return txt
 
+shout("SHOUT LOUD AND CLEAR")
+shout(5)
+shout(["Happy", "Days"])
 
 ## reverse all characters in string
 def reverse(txt):
-
+    if type(txt) in [int, float]:
+        raise TypeError("This is an integer or float. Input should be a single string.")
+    if type(txt) not in [str]:
+        raise TypeError("This is not a string. Input should be a single string.")
+    septxt = [i for i in txt]
+    septxt.reverse()
+    revtxt = "".join(septxt)
+    return revtxt
+    
+reverse("abcd")
+reverse("SHOUT LOUD AND CLEAR")
+reverse(5)
+reverse(["Happy", "Days"])
+    
 
 ## reverse word order in string
 def reversewords(txt):
+    if type(txt) in [int, float]:
+        raise TypeError("This is an integer or float. Input should be a single string.")
+    if type(txt) not in [str]:
+        raise TypeError("This is not a string. Input should be a single string.")
+    sepwords = txt.split(" ")
+    sepwords.reverse()
+    revwords = " ".join(sepwords)
+    return revwords
+
+reversewords("I love you")    
+reversewords("SHOUT LOUD AND CLEAR")
+reversewords(5)
+reversewords(["Happy", "Days"])
 
 
 ## reverses letters in each word
 def reversewordletters(txt):
+    if type(txt) in [int, float]:
+        raise TypeError("This is an integer or float. Input should be a single string.")
+    if type(txt) not in [str]:
+        raise TypeError("This is not a string. Input should be a single string.")
+    sepwords = txt.split(" ")
+    output = []
+    for words in sepwords:
+        output.append(reverse(words))
+    return output
+
+reversewordletters("I love you")
+reversewordletters("SHOUT LOUD AND CLEAR")
+reversewordletters(5)
+reversewordletters(["Happy", "Days"])
+
+
+
+
+
+
 
 
 ## optional -- change text to piglatin.. google it!
