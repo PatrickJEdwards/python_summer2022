262d92c7a580f004eb6c88ab68360df34011fa3f
Patrick Edwards <edwards.p@wustl.edu>
2022-08-10 21:49:58 -0500

    update

---
+++ Day3/Lab/lab03_test.py
@@ -0,0 +1,21 @@
+import unittest
+from lab03 import * # needs to be same wd as lab03.py
+
+class labTests(unittest.TestCase):
+	
+	## fill in a few tests for each
+	## make sure to account for correct and incorrect input
+
+    def test_shout(self):
+
+    def test_reverse(self):
+
+    def test_reversewords(self):
+
+    def test_reversewordletters(self):
+
+    def test_piglatin(self):
+
+
+if __name__ == '__main__':
+  unittest.main()
