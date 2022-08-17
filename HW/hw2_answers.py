7a145e441fc59d2da68fb8e9417c3524d89e57ef
Patrick Edwards <edwards.p@wustl.edu>
2022-08-17 02:22:50 -0500

    Update

--- HW/hw2_answers.py
+++ HW/hw2_answers.py
@@ -43,12 +43,12 @@ os.chdir('C:\\Users\\edwar\\Documents\\GitHub\\python_summer2022\\HW')
 ###     1B. Isolate html element corresponding to individual speeches.
 ###     1C. Isolate only Biden speeches, skip other authors.
 ## STEP 2: Get Information
-###     1A. Define framework that accumulates info.
-###     1B. Collect 'Date of Spoken Address' information.
-###     1C. Collect 'Title' information.
-###     1D. Access actual remark page.
-###     1E. Collect 'full text of address/remarks' information.
-###     1F. Collect 'citation/footnote' information, if it exists.
+###     2A. Define framework that accumulates info.
+###     2B. Collect 'Date of Spoken Address' information.
+###     2C. Collect 'Title' information.
+###     2D. Access actual remark page.
+###     2E. Collect 'full text of address/remarks' information.
+###     2F. Collect 'citation/footnote' information, if it exists.
 ## STEP 3: Completeness checks.
 ###     3A. Ensure that search stops at January 20, 2020.
 ###     3B. Ensure search goes through entire archive.
@@ -56,8 +56,160 @@ os.chdir('C:\\Users\\edwar\\Documents\\GitHub\\python_summer2022\\HW')
 ## STEP 4: Create CSV file of data.
 
 
-
+#%%
 # Homework #2 Answer.
+
+# Preliminaries.
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import csv
+import os
+import random
+import time
+
+# Actual csv input.
+with open("HW02_Answers.csv", "w", newline='') as f:
+    w = csv.DictWriter(f, fieldnames = ("date", "title", "text", "citation", "footnote"))
+    w.writeheader()
+    # Create correct URL with 625 speeches.
+    web_address = "https://www.presidency.ucsb.edu/documents/app-categories/presidential/spoken-addresses-and-remarks"
+    items_per_page = "?items_per_page="
+    goal_url = web_address + items_per_page + "625"
+    del web_address
+    del items_per_page
+    # open and parse website.
+    web_page = urllib.request.urlopen(goal_url)
+    del goal_url
+    soup = BeautifulSoup(web_page.read())
+    del web_page
+    # Isolate individual remarks.
+    divtags = soup.find_all("div", {"class" : "views-row"})
+    del soup
+    # Isolate only speeches with Biden as author.
+    all_remarks = []
+    for i in divtags:
+        if "Biden" in i.find_all("a")[1].text:
+            all_remarks.append(i)
+        else:
+            pass
+    del divtags
+    # Create counter.
+    counter = 0
+    # Create initial (pre-extension) part of URL.
+    pre_URL = "https://www.presidency.ucsb.edu"
+    
+##### Generate for-loop for finding data.
+    for i in all_remarks:
+        # Update counter.
+        counter += 1
+######### Print update using counter.
+        print("Working on " + str(counter) + " of " + str(len(all_remarks)))
+        # Create empty dictionary to fill in.
+        remark = {}
+        # Find extension to remarks personal page.
+        extension = i.find_all("div", {"class" : "field-title"})[0].find_all("a")[0]["href"]
+        
+###     2B. Collect 'Date of Spoken Address' information.
+        remark["date"] = i.find("span", {"class" : "date-display-single"}).text
+    
+###     2C. Collect 'Title' information.
+        remark["title"] = i.find("div", {"class" : "field-title"}).text[1:]
+
+###     2D. Access actual remark page.
+        try:
+            remark_page = urllib.request.urlopen(pre_URL + extension)
+        except urllib.error.URLError:
+            remark["text"] = "NA"
+            remark["citation"] = "NA"
+            w.writerow(remark)
+            continue
+        remark_html = BeautifulSoup(remark_page.read())
+        
+###     2E. Collect 'full text of address/remarks' information.
+        remark["text"] = remark_html.find_all("div", {"class" : "field-docs-content"})[0].text[1:]
+
+
+###     2F. Collect 'citation/footnote' information, if it exists.
+        try:
+            remark["citation"] = remark_html.find_all("div", {"class" : "field-prez-document-citation"})[0].text[1:]
+        except:
+            remark["citation"] = "NA"
+            continue
+        try:
+            remark["footnote"] = remark_html.find_all("div", {"class" : "field-docs-footnote"})[0].text
+        except:
+            remark["footnote"] = "NA"
+            continue
+        w.writerow(remark)
+        time.sleep(random.uniform(1, 3)) # be polite, sleep!
+print("ALL DONE!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+### IGNORE EVERYTHING BELOW THIS.
+### IGNORE EVERYTHING BELOW THIS.
+### IGNORE EVERYTHING BELOW THIS.
+### IGNORE EVERYTHING BELOW THIS.
+### IGNORE EVERYTHING BELOW THIS.
+### IGNORE EVERYTHING BELOW THIS.
+### IGNORE EVERYTHING BELOW THIS.
+### IGNORE EVERYTHING BELOW THIS.
+### IGNORE EVERYTHING BELOW THIS.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#t_extension = test_remarks[17].find_all("div", {"class" : "field-title"})[0].find_all("a")[0]["href"]
+#t_remark_page = urllib.request.urlopen(pre_URL + t_extension)
+#t_remark_html = BeautifulSoup(t_remark_page.read())
+
+# Text.
+#t_text = t_remark_html.find_all("div", {"class" : "field-docs-content"})[0].text[1:]
+#t_text
+
+#preprocessed_text = t_remark_html.find_all("div", {"class" : "field-docs-content"})[0].text[1:]
+#processed_text = preprocessed_text.replace("—","-").replace("–","-")
+#processed_text
+
+
+# citation.
+#t_remark_html.find_all("div", {"class" : "field-prez-document-citation"})[0].text[1:]
+#    "p", {"class" : "ucsbapp_citation"})[0].text
+
+# footnote.
+#t_remark_html.find_all("div", {"class" : "field-docs-footnote"})[0].text[1:]
+
+
+
 
 
 #%%
@@ -130,22 +282,108 @@ for i in divtags:
         divtags2.append(i)
     else:
         pass
-divtags = divtags2
-del divtags2
+del divtags
 ## STEP 2C FINISHED.
 
 
 
+#%%
 ## STEP 2: Get Information.
 
-###     1A. Define framework that accumulates info.
+###     2A. Define framework that accumulates info.
+
+# Make general outline for CSV creation.
+# Preliminaries.
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import csv
+import os
+import random
+import time
+# Actual csv input.
+with open("HW02_Answers.csv", "w", newline='') as f:
+    w = csv.DictWriter(f, fieldnames = ("date", "title", "text", "citation"))
+    w.writeheader()
+    # Create correct URL with 625 speeches.
+    web_address = "https://www.presidency.ucsb.edu/documents/app-categories/presidential/spoken-addresses-and-remarks"
+    items_per_page = "?items_per_page="
+    goal_url = web_address + items_per_page + "625"
+    del web_address
+    del items_per_page
+    # open and parse website.
+    web_page = urllib.request.urlopen(goal_url)
+    del goal_url
+    soup = BeautifulSoup(web_page.read())
+    del web_page
+    # Isolate individual remarks.
+    divtags = soup.find_all("div", {"class" : "views-row"})
+    del soup
+    # Isolate only speeches with Biden as author.
+    all_remarks = []
+    for i in divtags:
+        if "Biden" in i.find_all("a")[1].text:
+            all_remarks.append(i)
+        else:
+            pass
+    del divtags
+    # Create counter.
+    counter = 0
+    # Create initial (pre-extension) part of URL.
+    pre_URL = "https://www.presidency.ucsb.edu"
+    
+##### TEST: first 10 speeches.
+    test_remarks = []
+    for i in range(0, 10):
+        test_remarks.append(all_remarks[i])
+    del i
+
+##### Generate for-loop for finding data.
+    for i in test_remarks:
+        # Update counter.
+        counter += 1
+######### Print update using counter.
+        print("Working on " + str(counter) + " of " + str(len(test_remarks)))
+        # Create empty dictionary to fill in.
+        remark = {}
+        # Find extension to remarks personal page.
+        extension = i.find_all("div", {"class" : "field-title"})[0].find_all("a")[0]["href"]
+        
+###     2B. Collect 'Date of Spoken Address' information.
+        remark["date"] = i.find("span", {"class" : "date-display-single"}).text
+    
+###     2C. Collect 'Title' information.
+        remark["title"] = i.find("div", {"class" : "field-title"}).text[1:]
+
+###     2D. Access actual remark page.
+        try:
+            remark_page = urllib.request.urlopen(pre_URL + extension)
+        except urllib.error.URLError:
+            remark["text"] = "NA"
+            remark["citation"] = "NA"
+            w.writerow(remark)
+            continue
+        remark_html = BeautifulSoup(remark_page.read())
+        
+###     2E. Collect 'full text of address/remarks' information.
+        remark["text"] = remark_html.find_all("div", {"class" : "field-docs-content"})[0].text[1:]
 
 
+###     2F. Collect 'citation/footnote' information, if it exists.
+        try:
+            remark["citation"] = remark_html.find_all("p", {"class" : "ucsbapp_citation"})[0].text
+        except AttributeError:
+            remark["citation"] = "NA"
+        w.writerow(remark)
+        time.sleep(random.uniform(1, 5)) # be polite, sleep!
+print("ALL DONE!")
 
+#t_extension = test_remarks[0].find_all("div", {"class" : "field-title"})[0].find_all("a")[0]["href"]
+#t_remark_page = urllib.request.urlopen(pre_URL + t_extension)
+#t_remark_html = BeautifulSoup(t_remark_page.read())
 
-###     1B. Collect 'Date of Spoken Address' information.
-###     1C. Collect 'Title' information.
-###     1D. Access actual remark page.
-###     1E. Collect 'full text of address/remarks' information.
-###     1F. Collect 'citation/footnote' information, if it exists.
+#t_remark_html
+
+#remark_html.find_all("p", {"class" : "ucsbapp_citation"})[0].text
+
 
